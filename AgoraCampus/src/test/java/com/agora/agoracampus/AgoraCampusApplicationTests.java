07860db0c15ed546d0df30b79a0494760803a56d
Raresssm <lungurares4@gmail.com>
2026-04-25 13:41:20 +0300

    initialize AgoraCampus project with Maven setup and basic application structure

---
+++ AgoraCampus/src/test/java/com/agora/agoracampus/AgoraCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agora.agoracampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoraCampusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
